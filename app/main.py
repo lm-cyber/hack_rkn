49f419a7604f5c2d919f5fbf36ce594f5bca4db2
Nasibullin Alan Maratovich <alan.nasibyllin@yandex.ru>
2024-11-08 14:31:03 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -70,54 +70,54 @@ async def get_image(image_id: int):
 @app.put("/images/{image_id}")
 async def update_image(image_id: int, file: UploadFile = File(...)):
     db = await get_db()
-    minio_client = await get_minio_client()
+    async with await get_minio_client() as minio_client:
 
-    try:
-        image = await db.fetchrow("SELECT * FROM images WHERE id = $1", image_id)
-        if image is None:
-            raise HTTPException(status_code=404, detail="Image not found")
+        try:
+            image = await db.fetchrow("SELECT * FROM images WHERE id = $1", image_id)
+            if image is None:
+                raise HTTPException(status_code=404, detail="Image not found")
 
-        minio_path = f"{file.filename}"
-        
-        # Update MinIO
-        await minio_client.put_object(
-            Bucket="images",
-            Key=minio_path,
-            Body=file.file,
-            ContentType=file.content_type
-        )
+            minio_path = f"{file.filename}"
+            
+            # Update MinIO
+            await minio_client.put_object(
+                Bucket="images",
+                Key=minio_path,
+                Body=file.file,
+                ContentType=file.content_type
+            )
 
-        # Update PostgreSQL metadata
-        await db.execute(
-            """
-            UPDATE images SET filename = $1, minio_path = $2, content_type = $3 WHERE id = $4
-            """,
-            file.filename,
-            minio_path,
-            file.content_type,
-            image_id
-        )
-    finally:
-        await db.close()
+            # Update PostgreSQL metadata
+            await db.execute(
+                """
+                UPDATE images SET filename = $1, minio_path = $2, content_type = $3 WHERE id = $4
+                """,
+                file.filename,
+                minio_path,
+                file.content_type,
+                image_id
+            )
+        finally:
+            await db.close()
 
-    return {"filename": file.filename, "path": minio_path}
+        return {"filename": file.filename, "path": minio_path}
 
 @app.delete("/images/{image_id}")
 async def delete_image(image_id: int):
     db = await get_db()
-    minio_client = await get_minio_client()
+    async with await get_minio_client() as minio_client:
 
-    try:
-        image = await db.fetchrow("SELECT * FROM images WHERE id = $1", image_id)
-        if image is None:
-            raise HTTPException(status_code=404, detail="Image not found")
+        try:
+            image = await db.fetchrow("SELECT * FROM images WHERE id = $1", image_id)
+            if image is None:
+                raise HTTPException(status_code=404, detail="Image not found")
 
-        # Delete from MinIO
-        await minio_client.delete_object(Bucket="images", Key=image['minio_path'])
+            # Delete from MinIO
+            await minio_client.delete_object(Bucket="images", Key=image['minio_path'])
 
-        # Delete from PostgreSQL
-        await db.execute("DELETE FROM images WHERE id = $1", image_id)
-    finally:
-        await db.close()
+            # Delete from PostgreSQL
+            await db.execute("DELETE FROM images WHERE id = $1", image_id)
+        finally:
+            await db.close()
 
-    return {"detail": "Image deleted"}
+        return {"detail": "Image deleted"}
